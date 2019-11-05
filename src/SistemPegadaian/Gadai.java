1a86e3420917b0653d355f8db26580f9f093b9df
Tiaragprahasiwi <tiaragp88@gmail.com>
2019-11-05 15:33:14 +0700

    CreateInputGadai
    Menambahkan inputan menu Gadai (berupa nama, kategori, deskripsi, harga)

---
+++ src/SistemPegadaian/Gadai.java
@@ -0,0 +1,37 @@
+package SistemPegadaian;
+
+import java.util.Scanner;
+
+public class Gadai {
+
+	static String nama;
+	static String kategori;
+	static String deskripsi;
+	static int harga;
+	
+	public static void menuGadai(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Masukkan form dibawah ini :");
+		System.out.println("Nama :");
+		nama = input.next();
+		System.out.println("Product Category :");
+		kategori = input.next();
+		System.out.println("Deskripsi :");
+		deskripsi = input.next();
+		System.out.println("Harga :");
+		harga = input.nextInt();
+		
+		System.out.println();
+		System.out.println("SURAT BUKTI GADAI");
+		System.out.format("========================================================%n");
+		System.out.println("Nama\t\t\t: " + nama);
+		System.out.println("Product Category\t: " + kategori);
+		System.out.println("Deskripsi\t\t: " + deskripsi);
+		System.out.println("Harga\t\t\t: " + harga);
+		
+	}
+	public static void main(String[] args) {
+		menuGadai();
+	}
+
+}
