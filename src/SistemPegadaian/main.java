bb4a63a9b54af2a52553677fb62e5c500d4c8b70
honestynabila12 <57379335+honestynabila12@users.noreply.github.com>
2019-11-05 16:14:16 +0700

    Update menu Interface
    Update menu input dan tampilan

--- src/SistemPegadaian/main.java
+++ src/SistemPegadaian/main.java
@@ -10,31 +10,34 @@ public class main {
 		
 		do {	
 			try {
+				System.out.format(" ==============================================================================%n");
+				System.out.println(" ||===============*\t P E G A D A I A N   S Y S T E M \t*============||");
+				System.out.println(" ||==================*\t\t   KELOMPOK 3\t\t*====================||");
 				System.out.format("==============================================================================%n");
-				System.out.println("||====================\t P E G A D A I A N   S Y S T E M \t============||");
-				System.out.println("||====================\t\t   KELOMPOK 3\t\t====================||");
-				System.out.format("==============================================================================%n");
-				System.out.println("1. Gadai");
-				System.out.println("2. Tebus");
-				System.out.println("3. Tampilkan Inventory");
-				System.out.println("4. Exit");
+				System.out.println(" 1. Gadai");
+				System.out.println(" 2. Tebus");
+				System.out.println(" 3. Tampilkan Inventory");
+				System.out.println(" 4. Exit");
+				System.out.println();
 				
-				System.out.print("Masukan pilihan menu : ");
+				System.out.print("Masukan opsi pilihan : ");
 				pilihan = input.nextInt();
+				System.out.println();
+				
 				if (pilihan == 1) {
-					System.out.println("==================List Karyawan=================");
+					System.out.println("********************************* Menu Gadai *********************************\n\n");
 				}else if (pilihan == 2) {
-					System.out.println("=====================List Jadwal=====================");
+					System.out.println("******************************** Menu Tebus ***********************************\n\n");
 				}else if (pilihan == 3) {
-					System.out.println("===================List Nilai====================");
+					System.out.println("******************************** Inventory *************************************\n\n");
 				}else if (pilihan == 4) {
 					break;
 				}else {
-					System.out.println("\nPeringatan : Masukan pilihan angka 1 - 4!\n");
+					System.out.println("\nPeringatan : Angka yang Anda masukkan salah!!\n");
 				}
 			} catch (Exception e) {
 				input.nextLine();
-				System.out.println("\nPeringatan : Masukan pilihan angka 1 - 4!\n");
+				System.out.println("\nPeringatan : Angka yang Anda masukkan salah!\n");
 			}
 		} while (pilihan > 1 || pilihan < 4);
 	}
